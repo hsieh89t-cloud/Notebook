e7908bfc8f18de1fee1f5198f6751ffede00d6f8
hsieh89t-cloud <hsieh89t@gmail.com>
2025-10-01 06:13:50 +0800

    Add files via upload

--- config.js
+++ config.js
@@ -1,5 +1,1 @@
-
-window.__CONFIG__ = {
-  BASE_URL: "https://script.google.com/macros/s/AKfycbx3oTDF49EmwGp5ZgGs9WIO64m7UzVzj5IDcBmm222aR7eTQTG8DXUsYVGVnzH2ga4abQ/exec",
-  APP_TITLE: "Notebook"
-};
+window.__CONFIG__={BASE_URL:"https://script.google.com/macros/s/AKfycbx3oTDF49EmwGp5ZgGs9WIO64m7UzVzj5IDcBmm222aR7eTQTG8DXUsYVGVnzH2ga4abQ/exec",APP_TITLE:"Notebook"};
