6a78fc1f232a311b3b2e8549da01f9392e795eca
hsieh89t-cloud <hsieh89t@gmail.com>
2025-10-01 21:50:44 +0800

    Delete config.js

--- config.js
+++
@@ -1,3 +0,0 @@
-// v1.5 設定：把下行網址換成你的 GAS /exec
-// 例如：const BASE_URL = "https://script.google.com/macros/s/AKfycbxxxx/exec";
-const BASE_URL = "https://script.google.com/macros/s/AKfycbx3oTDF49EmwGp5ZgGs9WIO64m7UzVzj5IDcBmm222aR7eTQTG8DXUsYVGVnzH2ga4abQ/exec";
