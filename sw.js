bdfd2df8a0b142b878e0a7c4aa69300fd31fe9c5
hsieh89t-cloud <hsieh89t@gmail.com>
2025-10-01 21:51:38 +0800

    Delete sw.js

--- sw.js
+++
@@ -1,9 +0,0 @@
-const CACHE='nb-pwa-fullfix4';
-self.addEventListener('install',e=>self.skipWaiting());
-self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
-self.addEventListener('fetch',e=>{
-  const url=new URL(e.request.url);
-  if(url.searchParams.get('type')){ return; } // API 走網路
-  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(resp=>{const cp=resp.clone(); caches.open(CACHE).then(c=>c.put(e.request,cp)); return resp;})));
-});
-self.addEventListener('message',e=>{ if(e.data&&e.data.type==='SKIP_WAITING') self.skipWaiting(); });
