2b78f45f73c0ed54f9d72dab06c55a366f3025b6
hsieh89t-cloud <hsieh89t@gmail.com>
2025-10-01 21:28:11 +0800

    Add files via upload

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 // 修改這個 URL 成你的 GAS 網址
-const BASE_URL = "https://script.google.com/macros/s/你的GAS網址/exec";
+const BASE_URL = "https://script.google.com/macros/s/https://script.google.com/macros/s/AKfycbx3oTDF49EmwGp5ZgGs9WIO64m7UzVzj5IDcBmm222aR7eTQTG8DXUsYVGVnzH2ga4abQ/exec";
