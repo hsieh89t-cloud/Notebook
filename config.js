bdcafd287c53565fdb4a716c58d35c7f417af0fd
hsieh89t-cloud <hsieh89t@gmail.com>
2025-10-02 22:23:37 +0800

    Delete config.js

--- config.js
+++
@@ -1,7 +0,0 @@
-// 環境設定（已幫你填好）
-window.NB_CONFIG = {
-  API: "https://script.google.com/macros/s/AKfycbx3oTDF49EmwGp5ZgGs9WIO64m7UzVzj5IDcBmm222aR7eTQTG8DXUsYVGVnzH2ga4abQ/exec",
-  SHEET_ID: "1EwMHfpwD9HMKm0JiFhi7bx1bLPxTgOYrs7gu8ITTi24",
-  DRIVE_FOLDER_ID: "10g4D7PcXHqaPt5KcoRQ2tTz0oaW1Q543",
-  LIMIT: 50
-};
